be88bdee581fb0bd96b9c741840a7ca572dcd433
arnagel <anagel@keh.com>
2021-09-08 16:14:58 -0400

    added read_csv method.

--- main.py
+++ main.py
@@ -12,12 +12,17 @@ in_data = './input_data/'
 out_data = './output_data/'
 input_file = ''
 output_file = ''
-csv_ext = 'csv'
+csv_ext = '.csv'
 csv_obj = ''
+total_files_in = 0
 
 
 def main(args):
     get_args(args)
+    print('Input file is: ', input_file)
+    print('Output file is: ', output_file)
+    read_csv(in_data + input_file + csv_ext)
+    print("Total Incoming Files: {}".format(total_files_in))
 
 
 def get_args(argv):
@@ -37,16 +42,20 @@ def get_args(argv):
             input_file = arg
         elif opt in ("-o", "--ofile"):
             output_file = arg
-    print('Input file is "', input_file)
-    print('Output file is "', output_file)
 
 
-def open_csv():
-    pass
-
-
-def read_csv():
-    pass
+def read_csv(csv_file):
+    global total_files_in
+    from csv import reader
+    # open file in read mode
+    with open(csv_file, 'r') as read_obj:
+        # pass the file object to reader() to get the reader object
+        csv_reader = reader(read_obj)
+        # Iterate over each row in the csv using reader object
+        for row in csv_reader:
+            total_files_in = total_files_in + 1
+            # row variable is a list that represents a row in csv
+            print(row)
 
 
 # Press the green button in the gutter to run the script.
