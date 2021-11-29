63853788675309a99c555907708c293366a2502a
arnagel <anagel@keh.com>
2021-11-29 17:50:14 -0500

    build item1, item2 check and helper functions. Test cases and tested.

--- classes/ijs_helper.py
+++ classes/ijs_helper.py
@@ -15,6 +15,7 @@ __github__ = 'https://github.com/arnagel/image_junk_snooper.git'
 # imports
 import logging
 import pprint
+import re
 import sys
 from typing import Union
 from datetime import datetime
@@ -24,3 +25,174 @@ class IJSHelper:
 
     def __init__(self):
         pass
+
+    def evaluate_model_item(self, file_name) -> dict:
+        """different formats of model/item/barcode filename:
+
+        barcode, model_id, item_id, seq, copy_1, copy_2, ?
+        , height, width, pre_text, post_text, other
+
+        Split by period (.)
+        Split by underscore (_)
+
+        item #1a = numeric with hyphen
+
+        item #2a = 01 - 20
+        item #2b = has x
+        item #2c = has single digit
+        item #2d = numeric len >= 6
+        item #2e = has ( and )
+
+        [item #3a = numeric, single digit]
+        [item #3b = alpha]
+        [item #3c = has x]
+
+        [item #4a = numeric, single digit]
+
+        [item #5a = numeric, single digit]
+
+        1780834-1958332_08.jpg: model_id-item_id_sequence
+        214510-2305437_05.jpg: model_id-item_id_sequence
+        381966-3106923_02_MFG.jpg: model_id-item_id_sequence_text
+        386759-3452409_03(1).jpg: model_id-item_id_sequence(copies)
+
+        330186-1_300x300.jpg: model_id-sequence_HxW
+        330307_1_1000x1000.jpg: model_id-sequence_HxW
+        331220_1_1000x1000_1.jpg: model_id-sequence_HxW_copies
+        331263-2882_470x313.jpg: model_id-?_HxW
+        330870_1_1000x1000_1_2.jpg : model_id-sequence_HxW_copies_copies
+
+        model_378836_417x417.jpg: text_model_HxW
+        model_378836_417x417_1.jpg: text_model_HxW_sequence
+        model341884.jpg: textmodel
+        model378835_500x500.jpg: textmodel_HxW
+        model378461_1_1000x1000.jpg: textmodel_sequence_HxW
+        model378307_2_1000x1000_1.jpg: textmodel_sequence_HxW_copies
+        model368174_1.jpg: textmodel_sequence
+        10000239654683_08.jpg: barcode
+        33.jpg - Other not applicable/Disregard
+        """
+        dict_out = {}
+        if file_name:
+            split_period = file_name.split('.')
+            pprint.pprint(f"Period Split: :{split_period}")
+            len_period = len(split_period)
+            if len_period <= 1 or len_period > 2:
+                dict_out["error"] = file_name
+                return dict_out
+            """We use only item 0 from the returned period list"""
+            name = split_period[0]
+            """Underscore split"""
+            split_name = name.split('_')
+            pprint.pprint(f"Underscore Split: :{split_name}")
+
+            """Evaluate the first item in the return underscore list"""
+            dict_name = self.eval_item_1(split_name[0])
+            dict_out.update(dict_name)
+            dict_name = self.eval_item_2(split_name[1])
+            dict_out.update(dict_name)
+
+            if 'error' in dict_out:
+                pprint.pprint(f"Eval model item: :{dict_out}")
+                return dict_out
+
+            pprint.pprint(f"Eval model item: :{dict_out}")
+            return dict_out
+        else:
+            dict_out["error"] = file_name
+            return dict_out
+
+    def eval_item_1(self, name) -> dict:
+        """
+        #1a1 = split hyphen
+        $1a2 = item 1 > 6 (model_id)
+        $1a3 = item 2 > 6 (item_id)
+        """
+        """check if we have a hyphen in the name"""
+        f_str = "-"
+        dict_item = {}
+        if f_str not in name:
+            """No hyphen 
+            item #1b = numeric len > 10
+            item #1c = alphanumeric
+            item #1d = alpha"""
+            len_name = len(name)
+            if name.isnumeric():
+                if len_name > 10:
+                    """Barcode"""
+                    dict_item["barcode"] = name
+                elif self.check_id(name):
+                    """model_id"""
+                    dict_item["model_id"] = name
+                else:
+                    dict_item["error"] = name
+            elif name.isalnum():
+                """We have something like model23455"""
+                temp = re.compile("([a-zA-Z]+)([0-9]+)")
+                res = temp.match(name).groups()
+                dict_item["model_id"] = res[1]
+            elif isinstance(name, str):
+                """nothing we can use"""
+                dict_item["error"] = name
+        else:
+            """We have a hyphen and need to split"""
+            split_hyphen = name.split('-')
+            if self.check_id(split_hyphen[0]):
+                dict_item["model_id"] = split_hyphen[0]
+            else:
+                dict_item["error"] = split_hyphen[0]
+            if self.check_id(split_hyphen[1]):
+                dict_item["item_id"] = split_hyphen[1]
+            else:
+                dict_item["error"] = split_hyphen[1]
+        return dict_item
+
+    def eval_item_2(self, name) -> dict:
+        """
+        item  # 2a = 01 - 20
+        item  # 2b = has x
+        item  # 2c = has single digit
+        item  # 2d = numeric len >= 6
+        item  # 2e = has ( and ) """
+        dict_out = {}
+        x_str = "x"
+        bracket_str = "("
+        if x_str in name:
+            pprint.pprint(f"Found x: {name}")
+            dict_out = self.get_height_width(name)
+        elif bracket_str in name:
+            pprint.pprint(f"Found brackets: {name}")
+            dict_out = self.get_sequence_copy(name)
+        elif len(name) > 5:
+            pprint.pprint(f"Found model id: {name}")
+            if self.check_id(name):
+                dict_out['model_id'] = name
+        elif int(name) in range(0, 99):
+            pprint.pprint(f"Found sequence: {name}")
+            dict_out = {'sequence': name}
+        else:
+            dict_out = {"error": f"No match for: {name}"}
+
+        pprint.pprint(f"Dict Out: {dict_out}")
+
+        return dict_out
+
+
+
+
+
+        pass
+
+    def get_sequence_copy(self, str_s_c) -> dict:
+        lst_s_c = str_s_c.split('(')
+        return {'sequence': lst_s_c[0], 'copy': lst_s_c[1].rstrip(')')}
+
+    def get_height_width(self, str_h_w) -> dict:
+        lst_h_w = str_h_w.split('x')
+        return {'height': lst_h_w[0], 'width': lst_h_w[1]}
+
+    def check_id(self, test_id) -> bool:
+        len_test_id = len(test_id)
+        if 6 <= len_test_id <= 8:
+            return True
+        return False
--- classes/test_ijs_helper.py
+++ classes/test_ijs_helper.py
@@ -9,18 +9,74 @@ import logging
 from datetime import date
 
 
-class MyTestCase(unittest.TestCase):
+class TestIJSHelper(unittest.TestCase):
     def setUp(self) -> None:
         log_data = "../log_data/log_files/"
         log_file_name = log_data + 'test_image_junk_snooper_' + str(date.today()) + '.log'
         logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s'
                             , filename=log_file_name, level=logging.DEBUG)
+        from ijs_helper import IJSHelper
+        self.ijs_helper = IJSHelper()
+
+        self.url_path_file = '../output_data/test_file_report'
 
     def tearDown(self) -> None:
         pass
 
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_evaluate_model_item(self):
+        file_names = ['my', 'big_black_bear.coder.jpg', '10000239654683_08.jpg', '33.jpg', '1780834-1958332_08',
+                      '214510-2305437_05.jpg', '381966-3106923_02_MFG.jpg', '386759-3452409_03(1).jpg',
+                      '330186-1_300x300.jpg', '330307_1_1000x1000.jpg', '331220_1_1000x1000_1.jpg',
+                      '331263-2882_470x313.jpg', '330870_1_1000x1000_1_2.jpg', 'model_378836_417x417.jpg',
+                      'model_378836_417x417_1.jpg', 'model341884.jpg', 'model378835_500x500.jpg',
+                      'model378461_1_1000x1000.jpg', 'model378307_2_1000x1000_1.jpg', 'model368174_1.jpg']
+        msg = 'Evaluation failed'
+        self.assertIsInstance(self.ijs_helper.evaluate_model_item(file_names[3]), dict, msg)
+
+    def test_eval_item_1(self):
+        # file_name = '33'
+        file_name = '1780834-1958332'
+        # file_name = '214510-2305437'
+        # file_name = '330186'
+        # file_name = '331263-2882'
+        # file_name = 'model'
+        # file_name = 'model341884'
+        # file_name = '10000239654683'
+        msg = 'Item 1 Evaluation failed'
+        self.assertIsInstance(self.ijs_helper.eval_item_1(file_name), dict, msg)
+
+    def test_eval_item_2(self):
+        # file_name = '08'      # sequence
+        # file_name = '03(1)'   # sequence(copy)
+        # file_name = '300x300' # Height x Width
+        # file_name = '1'       # sequence
+        file_name = '100'       # sequence
+        # file_name = '378836'  # model_id
+
+        msg = 'Item 2 Evaluation failed'
+        self.assertIsInstance(self.ijs_helper.eval_item_2(file_name), dict, msg)
+
+    def test_check_id(self):
+        test_id = '37883'         # fail
+        # test_id = '378836'      # pass
+        # test_id = '3788360'     # pass
+        # test_id = '37883601'    # pass
+        # test_id = '137883601'   # fail
+
+        msg = 'Check id failed'
+        self.assertTrue(self.ijs_helper.check_id(test_id), msg)
+
+    def test_get_height_width(self):
+        name = '300x300'
+
+        msg = 'Getting height and width failed'
+        self.assertIsInstance(self.ijs_helper.get_height_width(name), dict, msg)
+
+    def test_get_sequence_copy(self):
+        name = '04(2)'
+
+        msg = 'Getting sequence and copy failed'
+        self.assertIsInstance(self.ijs_helper.get_sequence_copy(name), dict, msg)
 
 
 if __name__ == '__main__':
